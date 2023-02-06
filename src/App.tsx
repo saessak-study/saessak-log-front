e2addd422974bb6cdd3fe2aaf05f9b2184f97758
ShimSungBo <shimdb96@gmail.com>
2023-02-06 23:02:13 +0900

    초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ fontWeight: 400 }}>새싹로그 시작</h1>
+      <h1 style={{ fontWeight: 400 }}>폰트적용</h1>
+    </div>
+  );
+}
+
+export default App;
