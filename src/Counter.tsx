ba0a2b7629d3fb1fe370d021ffce8cf896c41b94
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-07 12:47:27 +0900

    redux 설정

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,7 +4,7 @@ import { up } from './app/counterSlice';
 
 function Counter() {
   const dispatch = useDispatch();
-  const count = useSelector((state: { counter: { value: number } }) => {
+  const count = useSelector((state: { counter: { value: number } }): number => {
     //state의 최상위 객체로 counterSlice.reducer의 이름이 들어감, 이 경우 counter
     return state.counter.value;
   });
