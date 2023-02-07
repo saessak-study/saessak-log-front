86963f56f2be232691453115815bc589afaa5895
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-07 11:54:36 +0900

    오류

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1 style={{ fontWeight: 400 }}>새싹로그 시작</h1>
-      <h1 style={{ fontWeight: 400 }}>폰트적용</h1>
+      <h1 style={{ fontWeight: 400 }}>폰트적용33</h1>
     </div>
   );
 }
