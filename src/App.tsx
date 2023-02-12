e32b137b703a0e990df4503c8602c35484d4b417
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-12 11:39:40 +0900

    navbar 검색창

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Counter from './Counter';
 import './style/common.scss';
-import MainPage from './page/mainPage';
+import MainPage from './page/Main';
 
 const App = () => {
   return (
     <div>
-      <MainPage/>
+      <MainPage />
     </div>
   );
 };
