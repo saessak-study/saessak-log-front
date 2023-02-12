6b302273942e64968f540b27c090fa5b6c30c955
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-12 10:17:18 +0900

    시작중

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Counter from './Counter';
 import './style/common.scss';
+import MainPage from './page/mainPage';
 
 const App = () => {
   return (
     <div>
-      <h1 style={{ fontWeight: 400 }}>새싹로그 시작</h1>
-      <h1 style={{ fontWeight: 400 }}>폰트적용33</h1>
-      <Counter />
+      <MainPage/>
     </div>
   );
 };
---
+++ src/page/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>mainPage</div>
+  )
+}
+
+export default MainPage
