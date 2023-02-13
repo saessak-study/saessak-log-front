b9302aca31acce4b808a2550d92ecc8dd7e29244
Hayeong8957 <judy.hayoung.shin@gmail.com>
2023-02-13 18:10:33 +0900

    feat : add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+/* eslint-disable react/self-closing-comp */
 import React from 'react';
-import Counter from './Counter';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './style/common.scss';
 
+import Main from './layouts/Main';
+
+// 개인 작업 보기 위한 임시 라우터입니다.
+// 추후에 라우터 디렉토리 따로 설정해도 좋습니다.
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
