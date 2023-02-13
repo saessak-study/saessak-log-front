4be0d337feb062d69bbe605a40c34e26284e1ea9
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-13 13:12:09 +0900

    오전변경 저장중

--- src/page/mainPage.tsx
+++ src/page/mainPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaUserAlt } from 'react-icons/fa';
+import { AiOutlineSearch, AiFillHeart } from 'react-icons/ai';
+import { FaUserAlt, FaRegCommentDots } from 'react-icons/fa';
+
 import '../style/homePage.scss';
 
 const MainPage = () => {
@@ -8,7 +9,7 @@ const MainPage = () => {
     <div className='main_container'>
       <div className='top_nav'>
         <div className='nav_logo'>
-          <img src='img/logo_img.png' />
+          <img src='img/temp.png' />
           <img src='img/logo_title.png' />
         </div>
         <div className='search_bar'>
@@ -18,16 +19,71 @@ const MainPage = () => {
         </div>
         <div className='nav_userInfo'>
           <div className='user_circle'>
-            <FaUserAlt className='user_profile' />
+            <FaUserAlt className='main_userProfile' />
           </div>
           유저 닉네임
-          <div className='nav_modal'>
-            <ul>
-              <li>계정관리</li>
-              <li>내 활동</li>
-              <li>구독함</li>
-              <li className='modal_logout'>로그</li>
-            </ul>
+          <ul className='nav_modal'>
+            <li>계정관리</li>
+            <li>내 활동</li>
+            <li>구독함</li>
+            <li className='modal_logout'>로그아웃</li>
+          </ul>
+        </div>
+      </div>
+      <div className='main_toggleBtn'>
+        <div className='mainToggle_on'>좋아요순</div>
+        <div className='mainToggle_off'>댓글순</div>
+      </div>
+      <div className='card_container'>
+        <div className='main_card'>
+          <img src='img/poodle.jpg' className='card_img' />
+          <div className='card_content'>
+            <div className='main_userInfo'>
+              <FaUserAlt className='main_userProfile' />
+              <span>작성자 이름</span>
+            </div>
+            <div className='card_detail'>
+              <div className='main_viewLike'>
+                <AiFillHeart /> 311
+              </div>
+              <div className='main_viewComment'>
+                <FaRegCommentDots /> 100
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='main_card'>
+          <img src='img/poodle.jpg' className='card_img' />
+          <div className='card_content'>
+            <div className='main_userInfo'>
+              <FaUserAlt className='main_userProfile' />
+              <span>작성자 이름</span>
+            </div>
+            <div className='card_detail'>
+              <div className='main_viewLike'>
+                <AiFillHeart /> 311
+              </div>
+              <div className='main_viewComment'>
+                <FaRegCommentDots /> 100
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='main_card'>
+          <img src='img/poodle.jpg' className='card_img' />
+          <div className='card_content'>
+            <div className='main_userInfo'>
+              <FaUserAlt className='main_userProfile' />
+              <span>작성자 이름</span>
+            </div>
+            <div className='card_detail'>
+              <div className='main_viewLike'>
+                <AiFillHeart /> 311
+              </div>
+              <div className='main_viewComment'>
+                <FaRegCommentDots /> 100
+              </div>
+            </div>
           </div>
         </div>
       </div>
