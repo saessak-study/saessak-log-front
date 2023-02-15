8f00d8a90cf03f79a9d8a5c5646b25a5a713a84c
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-02-15 19:23:37 +0900

    Main page머지 (#3)
    * main
    
    * 수정
    
    * 연습
    
    * 메인 페이지 변경
    
    * 오전변경 저장중
    
    * 메인페이지 임시 완성
    
    * 일단 오전 정리
    
    * chore : parcel.d.ts 추가
    
    * reafactor 폴더 구조 변경
    
    ---------
    
    Co-authored-by: Hayeong Shin <judy.hayoung.shin@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Counter from './Counter';
 import './style/common.scss';
+import MainPage from './components/main/mainPage';
+import SearchPage from './components/main/SearchPage';
 
 const App = () => {
   return <div></div>;
