56b6d683f47f1e7f3a095f18bb567e535696ac18
hyebin829 <smilerain779@gmail.com>
2023-02-16 21:59:11 +0900

    refactor : browser router 중복제거

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,12 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
