238cb2330ef3ef5ae9180571f542c99fbfe6cd7b
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-02-18 12:13:40 +0900

    Layout (#7)
    * 초기 세팅
    
    * 오류확인
    
    * 오류 확인
    
    * 오류
    
    * redux 세팅 완료
    
    * reducer 적용
    
    * redux 설정
    
    * redux 세팅
    
    * common scss 수정
    
    * common 추가
    
    * aaa
    
    * eslint, prettier 설정 추가 (#1)
    
    * Add : eslint, prettier 설정
    
    * Fix : eslint error
    
    * 시작중
    
    * navbar 검색창
    
    * 생성된 메인페이지 삭제
    
    * 메인페이지 삭제
    
    * Main page머지 (#3)
    
    * main
    
    * 수정
    
    * 연습
    
    * 메인 페이지 변경
    
    * 오전변경 저장중
    
    * 메인페이지 임시 완성
    
    * 일단 오전 정리
    
    * chore : parcel.d.ts 추가
    
    * reafactor 폴더 구조 변경
    
    ---------
    
    Co-authored-by: Hayeong Shin <judy.hayoung.shin@gmail.com>
    
    * Feature/post page : 게시글 보기 & 게시글 작성 레이아웃 (#5)
    
    * chore : Delete icon
    
    * chore : install react-router-dom
    
    * feat : add route
    
    * feat : create modal container
    
    * feat : create modal container
    
    * feat : temporary main page
    
    * feat : add @media
    
    * chore
    
    * feat : common modal component
    
    * feat : Edit common modal
    
    * feat : Edit style modal
    
    * feat : Edit style readPost
    
    * feat : Add some constants
    
    * feat : Edit Main page
    
    * feat : Create writePost modal
    
    * feat : Add mobile css
    
    * feat : Add common modal css
    
    * feat : Eidt readPost style(Add middle point)
    
    * feat : Add global font
    
    * feat : Edit readPost style
    
    * feat : Edit WritePost style
    
    * feat : Creat ReadPost & WritePost Layout
    
    * refactor : module.scss 적용 & 파일명 수정
    
    * refactor : 디렉토리 구조 변경
    
    * refactor : Edit for eslint-prettier error
    
    * chore : package.json 버전 원상복구 & App.tsx 코드 설명 주석
    
    * refactor : delete css folder
    
    * feat parcel d ts 삭제
    
    * feat dev머지 전
    
    * fff
    
    * dddd
    
    * ddd
    
    ---------
    
    Co-authored-by: ShimSungBo <shimdb96@gmail.com>
    Co-authored-by: hyebin829 <80376561+hyebin829@users.noreply.github.com>
    Co-authored-by: Hayeong Shin <judy.hayoung.shin@gmail.com>

---
+++ src/components/post/readPost/ReadPost.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Modal from '../modal/Modal';
+import styles from './readPost.module.scss';
+import { BsHeartFill, BsBookmark } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
+import { GrSend } from 'react-icons/gr';
+
+interface Props {
+  onClickToggleModal: () => void;
+}
+
+const ReadPost: React.FC<Props> = ({ onClickToggleModal }) => {
+  // const [showModal, setShowModal] = useState<boolean>(false);
+  return (
+    <div className={styles.container_wrapper}>
+      <Modal onClickToggleModal={onClickToggleModal} title='게시물'>
+        <div className={styles.img_container_web}>
+          <div className={styles.img} />
+        </div>
+        <div className={styles.non_img_container}>
+          <div className={styles.post_section}>
+            <div className={styles.post_section_header}>
+              <FaUserCircle className={styles.user_profile_img} />
+              <div className={styles.user_name}>사용자</div>
+              <BsBookmark className={styles.subscription_btn} />
+            </div>
+            <div className={styles.post_section_body}>
+              <div className={styles.img_container_mobile}>
+                <div className={styles.img} />
+              </div>
+              <div className={styles.text_section} />
+            </div>
+          </div>
+          <div className={styles.comment_read_section}>
+            <div className={styles.comment_component}>
+              <div className={styles.comment_user}>
+                <FaUserCircle className={styles.user_profile_img} />
+                <div className={styles.user_name_comment}>사용자</div>
+              </div>
+              <div className={styles.commnet_text}>전 이런 내용의 댓글을 달고 싶어요</div>
+            </div>
+            <div className={styles.comment_component}>
+              <div className={styles.comment_user}>
+                <FaUserCircle className={styles.user_profile_img} />
+                <div className={styles.user_name_comment}>사용자</div>
+              </div>
+              <div className={styles.commnet_text}>전 이런 내용의 댓글을 달고 싶어요</div>
+            </div>
+
+            <div className={styles.read_all_comments_btn}>댓글 모두 보기</div>
+          </div>
+          <div className={styles.comment_input_section}>
+            <div className={styles.like_section}>
+              <BsHeartFill className={styles.like_btn_icon} />
+              <div className={styles.like_count_number}>25</div>
+            </div>
+            <input className={styles.comment_input} placeholder='댓글을 작성하는 공간이에요' />
+            <GrSend className={styles.send_btn_icon} />
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default ReadPost;
