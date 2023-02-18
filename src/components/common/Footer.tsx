b1f0cc836114964b2ecca3b527704a3fd2d4d0dd
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-02-18 16:16:49 +0900

    Register to develop (#12)
    * 초기 세팅
    
    * 오류확인
    
    * 오류 확인
    
    * 오류
    
    * redux 세팅 완료
    
    * reducer 적용
    
    * redux 설정
    
    * redux 세팅
    
    * register 시작
    
    * reg input 완료전
    
    * aaa
    
    * color white
    
    * 저장
    
    * test
    
    ---------
    
    Co-authored-by: ShimSungBo <shimdb96@gmail.com>
    Co-authored-by: Dana Kim <124070996+danakim530@users.noreply.github.com>

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className={styles.infoContainer}>
+        <ul>
+          <li>새싹</li>
+          <li>로그</li>
+          <li>초보</li>
+          <li>개발</li>
+          <li>스터디</li>
+          <li>프로젝트</li>
+        </ul>
+        <select name="goto" id="goto">
+          <option value="">새싹스터디원</option>
+          <option value="">하영</option>
+          <option value="">길웅</option>
+          <option value="">다정</option>
+          <option value="">성보</option>
+          <option value="">혜빈</option>
+          <option value="">혜리</option>
+          <option value="">아연</option>
+          <option value="">호빈</option>
+          <option value="">건희</option>
+          <option value="">준호</option>
+          <option value="">성환</option>
+        </select>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
