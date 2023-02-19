55d1c998821fc6fa8dce71cfa435868104f6d150
Hayeong8957 <judy.hayoung.shin@gmail.com>
2023-02-19 17:48:01 +0900

    feature : Create PreviewImage component

---
+++ src/components/PreviewImage/PreviewImage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+// import { RootState } from './store';
+
+const PreviewImage = () => {
+  // const file = useSelector((state: RootState) => state.file);
+  // if (!file) {
+  //   return null;
+  // }
+  // const url = URL.createObjectURL(file);
+  // return (
+  //   <img src={url} alt="preview" />
+  // );
+};
+
+export default PreviewImage;
--- src/components/UploadImageButton/UploadImageButton.tsx
+++ src/components/UploadImageButton/UploadImageButton.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { BiImageAdd } from 'react-icons/bi';
+import React, { useRef } from 'react';
 import styles from './uploadImageButton.module.scss';
 
 const UploadImageButton = () => {
-  return <BiImageAdd className={styles.write_modal_add_img_icon} />;
+  const imgInputRef = useRef();
+  const imgUploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  // const imageUpload = () => {
+  //   const imgInputRef = useRef<HTMLInputElement>(null);
+  // };
+  const handleClickImageInput = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(e.target);
+  };
+
+  return (
+    <div className={styles.img_upload_btn_container}>
+      <input
+        className={styles.add_img_btn}
+        type='file'
+        accept='image/jpg, image/jpeg, image/png'
+        onChange={imgUploadHandler}
+      />
+    </div>
+  );
 };
 
 export default UploadImageButton;
