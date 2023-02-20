3fe58ec1db75287b5fc32f9c8adca229c692db03
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-20 14:12:38 +0900

    feat 메인페이지 레이아웃 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './style/common.scss';
-// import MainPage from './components/Main/mainPage';
 import SearchPage from './pages/Search/SearchPage';
 import Main from './pages/Main';
 import MyAccountPage from './pages/MyAccount/MyAccount';
 import MyActivity from './pages/MyActivity/MyActivity';
+import MainPage from './pages/Home/MainPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={<MainPage />} />
         <Route path='/search' element={<SearchPage />} />
         <Route path='/myaccount' element={<MyAccountPage />} />
         <Route path='/myactivity' element={<MyActivity />} />
