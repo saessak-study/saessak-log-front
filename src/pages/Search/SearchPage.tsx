cc8d19e89d063cb39daf5c5938082830f5da77ed
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-20 09:08:26 +0900

    feat 헤더 폴더구조 변경

--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -1,4 +1,4 @@
-import Header from '../../components/Header/Header';
+import Header from '../../components/common/Header';
 import SearchResult from '../../components/SearchResult/SearchResult';
 
 const SearchPage = () => {
