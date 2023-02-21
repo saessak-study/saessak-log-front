8fa4e5c1801bb3e60561877d7efa6e1ef227b33c
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-02-22 01:20:34 +0900

    헤더 우측 모달창 구현 (#18)
    * feat modal버튼 활성화
    
    * feat 헤더의 모달 토글 구현함
    
    * feat usestate부분 수정
    
    * feat 박스 명암사용
    
    * feat 네이밍과 searchResult 수정

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -6,12 +6,16 @@ const SearchResult = () => {
     <div className={styles.search_container}>
       <div className={styles.search_result}>
         <span style={{ fontSize: '32px', color: 'black' }}>
-          &quot;<span style={{ color: '#41d18a' }}>푸들</span>&quot;
+          &quot;<span style={{ color: '#41d18a' }}>고양이</span>&quot;
         </span>
         에 대한 검색 결과
       </div>
+      <div className={styles.searchBtn_wrap}>
+        <div className={styles.searchBtn_on}>좋아요순</div>
+        <div className={styles.searchBtn_off}>댓글순</div>
+      </div>
       {/* <div className={styles.searched_none}>일치하는 검색결과가 없습니다.</div> */}
-      <div className={styles.recent_searched}>
+      {/* <div className={styles.recent_searched}>
         <div className={styles.recent_searchedTitle}>
           <span style={{ fontSize: '32px' }}>최근 검색</span>
           <span className={styles.searched_eraseAll}>모두 지우기</span>
@@ -24,7 +28,7 @@ const SearchResult = () => {
             <span style={{ color: '#9a9a9a', cursor: 'pointer' }}>x</span>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
