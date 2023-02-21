8b23d153f640c219e0099dac1a246ca570e311a4
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-21 14:41:41 +0900

    feat modal버튼 활성화

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,13 @@
 import styles from './header.module.scss';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaUserAlt } from 'react-icons/fa';
+import { useState } from 'react';
 
 const Header = () => {
+  const [userModal, setUserModal] = useState(false);
+  const modalClick = () => {
+    setUserModal(!userModal);
+  };
   return (
     <div className={styles.top_nav}>
       <div className={styles.nav_logo}>
@@ -15,16 +20,18 @@ const Header = () => {
         <div className={styles.search_btn}>검색</div>
       </div>
       <div className={styles.nav_userInfo}>
-        <div className={styles.user_circle}>
+        <div className={styles.user_circle} onClick={modalClick} role='button' tabIndex={0}>
           <FaUserAlt className={styles.main_userProfile} />
         </div>
         유저 닉네임
-        <ul className={styles.nav_modal}>
-          <li>계정관리</li>
-          <li>내 활동</li>
-          <li>구독함</li>
-          <li className={styles.modal_logout}>로그아웃</li>
-        </ul>
+        {userModal === true ? null : (
+          <ul className={styles.nav_modal}>
+            <li>계정관리</li>
+            <li>내 활동</li>
+            <li>구독함</li>
+            <li className={styles.modal_logout}>로그아웃</li>
+          </ul>
+        )}
       </div>
     </div>
   );
