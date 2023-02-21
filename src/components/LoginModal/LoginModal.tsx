4876dcbd919bb36438292371d79adff62db55bda
danakim530 <danakim530@gmail.com>
2023-02-22 02:33:34 +0900

    feat Register&Login 유효성검사 alert기능 추가

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import Modal from '../Modal/ModalWide/ModalWide';
 import styles from './loginModal.module.scss';
+// eslint-disable-next-line import/no-cycle
 import RegisterModal from '../RegisterModal/RegisterModal';
+// eslint-disable-next-line import/no-cycle
 import FindPwModal from '../FindIdPwModal/FindPwModal';
+// eslint-disable-next-line import/no-cycle
 import FindIdModal from '../FindIdPwModal/FindIdModal';
+import { INFO_INVALID } from '../../constants/message';
+
+// alert_message 띄우기까지 완료(from 다정)
 
 interface Props {
   onClickToggleModal: () => void;
 }
 
 const LoginModal: React.FC<Props> = ({ onClickToggleModal }) => {
+  // show modal
   const [showFindIdModal, setShowFindIdModal] = useState<boolean>(false);
   const [showRegisterModal, setShowRegisterModal] = useState<boolean>(false);
   const [showFindPwModal, setShowFindPwModal] = useState<boolean>(false);
@@ -24,6 +31,38 @@ const LoginModal: React.FC<Props> = ({ onClickToggleModal }) => {
     setShowFindPwModal((prev) => !prev);
   };
 
+  // login
+  const [inputs, setInputs] = useState({
+    userId: '',
+    userPw: '',
+  });
+  const { userId, userPw } = inputs;
+  const [idValid, setIdValid] = useState(false);
+  const [pwValid, setpwValid] = useState(false);
+  const [isAlert, setIsAlert] = useState(false);
+
+  // input 추가
+  const onChangeInputs = (e: { target: { value: any; name: any } }) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  // id or pw error -> alert
+  const loginBtnAction = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (!idValid) {
+      setIsAlert(true);
+    } else if (!pwValid) {
+      setIsAlert(true);
+    } else {
+      setIsAlert(false);
+      // handleSubmit(e);
+    }
+  };
+
   return (
     <div>
       <Modal onClickToggleModal={onClickToggleModal} title='로그인'>
@@ -33,11 +72,33 @@ const LoginModal: React.FC<Props> = ({ onClickToggleModal }) => {
             더 많은 기능을 사용하시려면 로그인을 해주세요.
           </div>
           <div className={styles.login_inputComponent}>
-            <input type='text' className={styles.login_input} placeholder='ID' />
+            <input
+              name='userId'
+              maxLength={12}
+              value={userId}
+              type='text'
+              className={styles.login_input}
+              placeholder='ID'
+              onChange={onChangeInputs}
+            />
           </div>
           <div className={styles.login_inputComponent}>
-            <input type='password' className={styles.login_input} placeholder='Password' />
-            <div className={styles.login_errorMSG}>입력한 내용을 다시 확인해주세요.</div>
+            <input
+              name='userPw'
+              maxLength={16}
+              value={userPw}
+              type='password'
+              className={styles.login_input}
+              placeholder='Password'
+              onChange={onChangeInputs}
+            />
+            <div className={styles.login_errorMSG}>
+              <span>
+                {isAlert && !idValid && !pwValid && INFO_INVALID}
+                {isAlert && idValid && !pwValid && INFO_INVALID}
+                {!isAlert && idValid && pwValid ? '' : null}
+              </span>
+            </div>
           </div>
           <div className={styles.route_box}>
             <a
@@ -48,7 +109,6 @@ const LoginModal: React.FC<Props> = ({ onClickToggleModal }) => {
             >
               아이디 /
             </a>
-
             <a
               href='#!'
               type='button'
@@ -59,7 +119,9 @@ const LoginModal: React.FC<Props> = ({ onClickToggleModal }) => {
             </a>
           </div>
           <div className={styles.login_buttonBox}>
-            <div className={styles.login_normal}>로그인</div>
+            <button type='button' className={styles.login_normal} onClick={loginBtnAction}>
+              로그인
+            </button>
             <div className={styles.login_kakao}>카카오 로그인</div>
           </div>
           <label className={styles.autologin_radiobutton}>
