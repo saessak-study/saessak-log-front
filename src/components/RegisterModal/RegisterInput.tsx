34c1f6602c84992da8d6d89e2767143bb490fbb2
danakim530 <danakim530@gmail.com>
2023-02-22 21:38:30 +0900

    feat registerModal.module.scss 반응형 에러 수정

--- src/components/RegisterModal/RegisterInput.tsx
+++ src/components/RegisterModal/RegisterInput.tsx
@@ -4,17 +4,15 @@ import styles from './registerModal.module.scss';
 // eslint-disable-next-line react/prop-types
 const RegisterInput = ({ name, message, inputType, placeholder, onChange, value, showUpMSG }) => {
   return (
-    <div>
-      <div className={styles.reg_inputComponent}>
-        <input
-          className={styles.register_input}
-          type={inputType}
-          placeholder={placeholder}
-          onChange={onChange}
-          name={name}
-          value={value}
-        />
-      </div>
+    <div className={styles.reg_inputComponent}>
+      <input
+        className={styles.register_input}
+        type={inputType}
+        placeholder={placeholder}
+        onChange={onChange}
+        name={name}
+        value={value}
+      />
       <div className={styles.reg_errorMSG}>{showUpMSG ? message : null}</div>
     </div>
   );
