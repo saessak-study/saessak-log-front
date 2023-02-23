93865763760ecf42a4a0485c0b2f768fa18933e8
Hayeong Shin <judy.hayoung.shin@gmail.com>
2023-02-23 14:16:24 +0900

    feat : Edit Modal (#20)
    * feat 모달 토글 삭제
    
    * feat message.tsx 추가
    
    * feat 로그인, 회원가입 반응형 수정
    
    * feat onclick : Modal에서 다른 Modal로 이동>
    
    * feat Register&Login 유효성검사 alert기능 추가
    
    * feat registerModal.module.scss 반응형 에러 수정
    
    * feat modal onclick 수정&ModalOpen.tsx 버튼 하나 남기기
    
    * feat modal header 제거
    
    * feat footer 디자인수정
    
    ---------
    
    Co-authored-by: danakim530 <danakim530@gmail.com>

--- src/components/FindIdPwModal/FindIdModal.tsx
+++ src/components/FindIdPwModal/FindIdModal.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Modal from '../Modal/ModalWide/ModalWide';
+import react from 'react';
+import Modal from '../Modal/ModalNoHeader/ModalNoHeader';
 import styles from './findIdPwModal.module.scss';
 
 interface Props {
@@ -11,10 +11,6 @@ const FindIdModal: React.FC<Props> = ({ onClickToggleModal }) => {
     <div>
       <Modal onClickToggleModal={onClickToggleModal} title='아이디찾기'>
         <div className={styles.findIdPw_container}>
-          <div className={styles.findIdPw_buttonBox}>
-            <div className={styles.findIdPw_greenButton}>아이디</div>
-            <div className={styles.findIdPw_whiteButton}>비밀번호</div>
-          </div>
           <div className={styles.findIdPw_title}>아이디 찾기</div>
           <div className={styles.findIdPw_inputComponent}>
             <input type='text' className={styles.findIdPw_input} placeholder='ID' />
@@ -26,7 +22,6 @@ const FindIdModal: React.FC<Props> = ({ onClickToggleModal }) => {
           <div className={styles.findIdPw_buttonBox}>
             <div className={styles.findIdPw_greenButton}>아이디 찾기</div>
           </div>
-          <div className={styles.route_login}>로그인 화면으로 돌아가기</div>
         </div>
       </Modal>
     </div>
---
+++ src/components/RegisterModal/RegisterInput.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './registerModal.module.scss';
+
+// eslint-disable-next-line react/prop-types
+const RegisterInput = ({ name, message, inputType, placeholder, onChange, value, showUpMSG }) => {
+  return (
+    <div className={styles.reg_inputComponent}>
+      <input
+        className={styles.register_input}
+        type={inputType}
+        placeholder={placeholder}
+        onChange={onChange}
+        name={name}
+        value={value}
+      />
+      <div className={styles.reg_errorMSG}>{showUpMSG ? message : null}</div>
+    </div>
+  );
+};
+
+export default RegisterInput;
