a81be27ac33efad88f5de2bdb823f633542d87e6
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-02-23 10:50:50 +0900

    feat navigate 생성

--- src/components/SideRouteBtn/SideRouteBtn.tsx
+++ src/components/SideRouteBtn/SideRouteBtn.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './sideRouteBtn.module.scss';
 import { FaUserAlt } from 'react-icons/fa';
 import { BiPlusMedical } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
+import WritePost from '../WritePost/WritePost';
 
 const SideRouteBtn = () => {
+  let navigate = useNavigate();
+  const [showWriteModal, setShowWriteModal] = useState<boolean>(false);
+  const onClickWritePost = () => {
+    setShowWriteModal((prev) => !prev);
+  };
+
   return (
     <aside>
       <div className={styles.sideBtn}>
-        <div className={styles.route_user}>
+        <div
+          className={styles.route_user}
+          onClick={() => {
+            navigate('/myaccount');
+          }}
+          role='button'
+          tabIndex={0}
+        >
           <FaUserAlt />
         </div>
-        <div className={styles.route_add}>
+        <div className={styles.route_add} onClick={onClickWritePost} role='button' tabIndex={0}>
           <BiPlusMedical />
         </div>
+        {showWriteModal && <WritePost onClickToggleModal={onClickWritePost} />}
       </div>
     </aside>
   );
