7e1086a38d22f9659424fca9ad2c47dfe849410e
Hayeong8957 <judy.hayoung.shin@gmail.com>
2023-02-25 17:07:04 +0900

    refactor/Add-uploadPost-headers

--- src/actions/uploadPost.ts
+++ src/actions/uploadPost.ts
@@ -4,7 +4,10 @@ import { uploadPostState } from '../types/post';
 
 axios.defaults.baseURL = 'http://52.78.251.23:8080';
 
+const headers = {
+  // Authorization: `Bearer ${토큰}`
+};
 export const uploadPost = createAsyncThunk('/posts/new', async (data: uploadPostState) => {
-  const response = await axios.post('/posts/new');
+  const response = await axios.post('/posts/new', data, { headers });
   return response.data;
 });
