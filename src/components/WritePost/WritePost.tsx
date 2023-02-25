4297c197494bf0621c9fc542893809b0e0b307e3
Hayeong8957 <judy.hayoung.shin@gmail.com>
2023-02-25 17:39:06 +0900

    필요없는 코드 삭제

--- src/components/WritePost/WritePost.tsx
+++ src/components/WritePost/WritePost.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setImageFile, setPostText } from '../../reducers/uploadPost';
-import { uploadPostState } from '../../types/post';
 import { uploadPost } from '../../actions/uploadPost';
 import Modal from '../Modal/Modal';
 import styles from './writePost.module.scss';
