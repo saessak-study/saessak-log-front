c2ed612010f9c3cd94d5fc3277080602090cb94d
hyebin829 <smilerain779@gmail.com>
2023-02-26 19:43:58 +0900

    refactor : link button

--- src/components/PasswordChangeModal/PasswordChangeModal.tsx
+++ src/components/PasswordChangeModal/PasswordChangeModal.tsx
@@ -7,9 +7,9 @@ const PasswordChangeModal = () => {
       <div className={styles.message_wrapper}>
         비밀번호가 변경되었습니다.
         <br />새 비밀번호로 다시 로그인해주세요.
-        <button type='button'>
-          <Link to='/'>로그인 하러 가기</Link>
-        </button>
+        <Link to='/'>
+          <button type='button'>로그인 하러 가기</button>
+        </Link>
       </div>
     </div>
   );
