d2bdb7511aca4b7ed8d1e89a6150af4c4e4a3763
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2023-02-28 12:51:23 +0900

    feat 페이징 시작

--- src/components/CardList/CardEach.tsx
+++ src/components/CardList/CardEach.tsx
@@ -3,17 +3,19 @@ import { MdOutlineComment } from 'react-icons/md';
 import styles from './CardList.module.scss';
 import { useState } from 'react';
 import { cardProfileType } from '../../types/card';
+import ReadPost from '../ReadPost/ReadPost';
 
 const CardEach = ({ cardProfile }: cardProfileType) => {
-  const [showReadModal, setShowReadModal] = useState<boolean>(true);
+  const [showReadModal, setShowReadModal] = useState<boolean>(false);
 
   const onClickReadPost = () => {
+    console.log(showReadModal);
     setShowReadModal((prev) => !prev);
   };
 
   return (
     <ul className={styles.cardWrapper}>
-      {/* {showReadModal && <ReadPost onClickToggleModal={onClickReadPost} />} */}
+      {showReadModal && <ReadPost onClickToggleModal={onClickReadPost} />}
       <li className={styles.imageWrapper}>
         <img src={cardProfile.imageFile} alt='post' />
       </li>
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -4,55 +4,19 @@ import { getCardList } from '../../actions/viewCardList';
 import { useAppDispatch, useAppSelector } from '../../hooks/useRedux';
 import { useEffect, useState } from 'react';
 
-const dummyData = [
-  {
-    id: 1,
-    imgsrc: 'https://cdn.pixabay.com/photo/2017/09/18/15/47/cat-2762156_960_720.jpg',
-    like: 1,
-    comment: 2,
-  },
-  {
-    id: 2,
-    imgsrc: 'https://cdn.pixabay.com/photo/2017/09/18/15/47/cat-2762156_960_720.jpg',
-    like: 2,
-    comment: 25,
-  },
-  {
-    id: 3,
-    imgsrc: 'https://cdn.pixabay.com/photo/2017/09/18/15/47/cat-2762156_960_720.jpg',
-    like: 1,
-    comment: 2,
-  },
-  {
-    id: 4,
-    imgsrc: 'https://cdn.pixabay.com/photo/2017/09/18/15/47/cat-2762156_960_720.jpg',
-    like: 1,
-    comment: 2,
-  },
-  {
-    id: 5,
-    imgsrc: 'https://cdn.pixabay.com/photo/2017/09/18/15/47/cat-2762156_960_720.jpg',
-    like: 1,
-    comment: 2,
-  },
-  {
-    id: 6,
-    imgsrc: 'https://cdn.pixabay.com/photo/2017/09/18/15/47/cat-2762156_960_720.jpg',
-    like: 1,
-    comment: 2,
-  },
-];
-
 const CardList = () => {
+  // 페이징 관련 상태관리
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
   const limit = 6;
   const page = 1;
   const params = { limit, page };
   const dispatch = useAppDispatch();
-  const { loadCardListError, loadCardList } = useAppSelector((state) => state.loadCardList);
+  // const { loadCardListError, loadCardList } = useAppSelector((state) => state.loadCardList);
   const [cardMainView, setCardMainView] = useState([]);
   const viewCardList = async () => {
     const result = await dispatch(getCardList(params));
-    console.log(result.payload);
     setCardMainView(result.payload);
   };
   useEffect(() => {
@@ -60,8 +24,8 @@ const CardList = () => {
   }, []);
   return (
     <div className={styles.cardListWrapper}>
-      {cardMainView.map((item) => {
-        return <CardEach key={item.id} cardProfile={item} />;
+      {cardMainView.map((item, i) => {
+        return <CardEach key={i} cardProfile={item} />;
       })}
     </div>
   );
