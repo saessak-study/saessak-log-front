4560dea4f9d3c0daff4f4010c44c43b69a8c8eaf
Hayeong8957 <judy.hayoung.shin@gmail.com>
2023-02-28 14:33:41 +0900

    스타일 수정

--- src/actions/uploadPost.ts
+++ src/actions/uploadPost.ts
@@ -4,15 +4,15 @@ import { uploadPostContents } from '../types/uploadpost';
 
 axios.defaults.baseURL = 'http://52.78.251.23:8080';
 const accessToken =
-  'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjIxMiIsInByb2ZpbGVJZCI6InF3ZXIxMjM1IiwiaWF0IjoxNjc3NDMwMjg0LCJleHAiOjE2Nzc0NDAzNjR9.y_v6RgYSI9OVp-y0txXkkskJq4uuUr2bTezEFZqzxpo';
+  'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjIxMiIsInByb2ZpbGVJZCI6InF3ZXIxMjM1IiwiaWF0IjoxNjc3NDc3MjI5LCJleHAiOjE2Nzc0ODczMDl9.rMpx2slXNHdfBpaxCVXfb4edxis8BOZEVYkTWHi2GqE';
 export const uploadPost = createAsyncThunk(
-  '/posts/new',
+  '/posts/new2',
   async (data: uploadPostContents, { rejectWithValue }) => {
     const formData = new FormData();
     formData.append('imageFile', data.imageFile);
     formData.append('postText', data.postText);
     try {
-      const response = await axios.post('/posts/new', formData, {
+      const response = await axios.post('/posts/new2', formData, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
           'Content-Type': 'multipart/form-data',
