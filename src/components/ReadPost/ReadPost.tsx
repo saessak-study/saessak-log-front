b8210f7f6f637cbe8f6ba3b006e9b4a398cdd502
Hayeong8957 <judy.hayoung.shin@gmail.com>
2023-03-01 17:11:15 +0900

    feat: Edit css readpost

--- src/components/ReadPost/ReadPost.tsx
+++ src/components/ReadPost/ReadPost.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../Modal/Modal';
 import HeartButton from '../HeartButton/HeartButton';
 import CommentList from '../CommentList/CommentList';
@@ -6,6 +6,7 @@ import styles from './readPost.module.scss';
 import { FaUserCircle } from 'react-icons/fa';
 import { BsBookmark } from 'react-icons/bs';
 import { GrSend } from 'react-icons/gr';
+import axios from 'axios';
 
 interface Props {
   onClickToggleModal: () => void;
@@ -13,32 +14,52 @@ interface Props {
 
 const ReadPost = ({ onClickToggleModal }: Props) => {
   const [likeCount, setLikeCount] = useState<number>(0);
-
+  const [viewImage, setViewImage] = useState<string>('');
+  const [viewPostText, setViewPostText] = useState<string>('');
+  const [viewUserId, setViewUserId] = useState<string>('');
+  const postId = 338;
   const onClickheartBtn = (isLike: boolean) => {
     if (isLike) setLikeCount(likeCount + 1);
     else setLikeCount(likeCount - 1);
   };
+  // likeCount https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/asdf"
+
+  useEffect(() => {
+    axios
+      .get(`/posts/${postId}`)
+      .then((response) => {
+        console.log(response);
+        setViewImage(response.data.imageFile);
+        setViewPostText(response.data.postText);
+        setViewUserId(response.data.profileId);
+        /** 좋아요 수는 전역 관리 해야함 */
+        setLikeCount(response.data.likeCount);
+      })
+      .catch((error) => {
+        console.log('Error: ', error.response.data.responseMessage);
+      });
+  }, []);
 
   return (
     <div className={styles.container_wrapper}>
       <Modal onClickToggleModal={onClickToggleModal} title='게시물'>
         <div className={styles.img_container_web}>
-          <div className={styles.img} />
+          <img src={viewImage} className={styles.img} />
         </div>
         <div className={styles.non_img_container}>
           <div className={styles.post_section}>
             <div className={styles.post_section_header}>
               <FaUserCircle className={styles.user_profile_img} />
-              <div className={styles.user_name}>사용자</div>
+              <div className={styles.user_name}>{viewUserId}</div>
               <BsBookmark className={styles.subscription_btn} />
             </div>
 
             <div className={styles.post_scroll_section}>
               <div className={styles.post_section_body}>
                 <div className={styles.img_container_mobile}>
-                  <div className={styles.img} />
+                  <img src={viewImage} className={styles.img} />
                 </div>
-                <div className={styles.text_section} />
+                <div className={styles.text_section}>{viewPostText}</div>
               </div>
               <div className={styles.comment_read_section}>
                 <CommentList />
