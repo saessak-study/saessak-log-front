18a4924cb85cbad21c45beb6ad27c7ba19532f3c
hyebin829 <smilerain779@gmail.com>
2023-03-02 16:09:35 +0900

    delete : 사용하지 않는 컴포넌트

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = '';
+axios.defaults.baseURL = 'http://52.78.251.23:8080';
 
-// export const loadPost = createAsyncThunk('', async () => {
-//   const response = await axios.get('');
-//   return response.data;
-// });
+export const loadMyPost = createAsyncThunk('/post/loadMyPost', async (data) => {
+  try {
+    const token = sessionStorage.getItem('token');
+    const response = await axios.get(`/posts/myActivity?limit=6&page=${1}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return console.error(error);
+  }
+});
