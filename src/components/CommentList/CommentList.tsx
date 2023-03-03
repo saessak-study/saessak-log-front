9997561a4fda87382bcf12acee7ba18f7212a91a
Hayeong Shin <judy.hayoung.shin@gmail.com>
2023-03-03 16:00:27 +0900

    Feature/upload comment (#41)
    * 초기 세팅
    
    * 오류확인
    
    * 오류 확인
    
    * 오류
    
    * redux 세팅 완료
    
    * reducer 적용
    
    * redux 설정
    
    * redux 세팅
    
    * common scss 수정
    
    * common 추가
    
    * aaa
    
    * eslint, prettier 설정 추가 (#1)
    
    * Add : eslint, prettier 설정
    
    * Fix : eslint error
    
    * 시작중
    
    * navbar 검색창
    
    * 생성된 메인페이지 삭제
    
    * 메인페이지 삭제
    
    * chore : Delete icon
    
    * chore : install react-router-dom
    
    * feat : add route
    
    * feat : create modal container
    
    * feat : create modal container
    
    * feat : temporary main page
    
    * feat : add @media
    
    * chore
    
    * feat : common modal component
    
    * feat : Edit common modal
    
    * feat : Edit style modal
    
    * feat : Edit style readPost
    
    * feat : Add some constants
    
    * feat : Edit Main page
    
    * feat : Create writePost modal
    
    * feat : Add mobile css
    
    * feat : Add common modal css
    
    * feat : Eidt readPost style(Add middle point)
    
    * feat : Add global font
    
    * feat : Edit readPost style
    
    * feat : Edit WritePost style
    
    * feat : Creat ReadPost & WritePost Layout
    
    * refactor : module.scss 적용 & 파일명 수정
    
    * refactor : 디렉토리 구조 변경
    
    * refactor : Edit for eslint-prettier error
    
    * Main page머지 (#3)
    
    * main
    
    * 수정
    
    * 연습
    
    * 메인 페이지 변경
    
    * 오전변경 저장중
    
    * 메인페이지 임시 완성
    
    * 일단 오전 정리
    
    * chore : parcel.d.ts 추가
    
    * reafactor 폴더 구조 변경
    
    ---------
    
    Co-authored-by: Hayeong Shin <judy.hayoung.shin@gmail.com>
    
    * chore : package.json 버전 원상복구 & App.tsx 코드 설명 주석
    
    * feat : Separate heartButton Component
    
    * feat : Separate heartButton Component
    
    * refactor : Separate CommentList & Edit style of ReadPost
    
    * chore : Delete css folder
    
    * chore : ignorecase true to false
    
    * refactor : Delete prettier error
    
    * feat : Edit heartBtn
    
    * feat : Create uploadimgbtn folder
    
    * refactor : Edit directory-structire & Edit scss error of @extend
    
    * feature : Create PreviewImage component
    
    * feat : Start create uploadimgbutton'
    
    * feat : Create ImgCropModal
    
    * feat : 초기 cropper
    
    * feat : utils/getCroppedImg 추가
    
    * refactor : Edit route
    
    * feat : commentList reducer
    
    * feat: 댓글 redux 세팅
    
    * feat: 댓글 불러와지는 거 확인
    
    * feat: 댓글 불러와지는 거 확인
    
    * 감자 감자 감자
    
    * feat: Edit css readpost
    
    * feat: commet pagenation
    
    * help me
    
    * refactor: infinite-scroll
    
    * refactor: infinite-scroll
    
    * refactor: Separate comment Input & writepost visible depending on token
    
    ---------
    
    Co-authored-by: ShimSungBo <shimdb96@gmail.com>
    Co-authored-by: DoggySummer <91588779+DoggySummer@users.noreply.github.com>
    Co-authored-by: hyebin829 <80376561+hyebin829@users.noreply.github.com>

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -3,59 +3,41 @@ import { useAppDispatch, useAppSelector } from '../../hooks/useRedux';
 import { loadComment } from '../../actions/comment';
 import { FaUserCircle } from 'react-icons/fa';
 import styles from './commentList.module.scss';
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
 
-const CommentList = () => {
+interface Props {
+  postID: number;
+}
+
+const CommentList = ({ postID }: Props) => {
   const dispatch = useAppDispatch();
-  const { loadCommentError, loadCommentList, loadCommentlimit } = useAppSelector(
+  const { loadCommentLoading, loadCommentList, hasMore, pageNum } = useAppSelector(
     (state) => state.loadComment
   );
-  const postId = 1;
-  const limit: number = 5;
-  const [page, setPage] = useState<number>(0); // 현재 페이지
-  const [moreLoad, setMoreLoad] = useState<boolean>(true); // 추가 댓글 있는 지
-  const params = { postId, limit, page };
+  const paramsTest = { postID: 1, pageNum }; // postID에 50개의 댓글이 있어 스크롤 확인시
+  const params = { postID, pageNum };
 
-  useEffect(() => {
-    loadComments();
-  }, [page]); // page가 변경될 때마다 useEffect 실행
-
-  const loadComments = () => {
-    dispatch(loadComment(params));
-    console.log(loadCommentList);
-
-    if (loadCommentError) {
-      alert('댓글 못 불러옴');
-    }
-    if (page === 0) {
-      setMoreLoad(true);
-    }
-    if (loadCommentlimit.length < limit) {
-      setMoreLoad(false);
-    }
-  };
-
-  const handleLoadMore = () => {
-    setPage((prev) => prev + 1); // 다음 페이지로 이동
-  };
+  const target = useInfiniteScroll({
+    hasMore,
+    loadData: () => dispatch(loadComment(paramsTest)),
+    isLoading: loadCommentLoading,
+  });
 
   return (
     <div>
-      {loadCommentList.map((comment, i) => (
-        <div key={i} className={styles.comment_component}>
-          <div className={styles.comment_user}>
-            <FaUserCircle className={styles.user_profile_img} />
-            <div className={styles.user_name_comment}>{comment.profileId}</div>
+      <div>
+        {loadCommentList.length === 0 && <div>댓글이 없습니다. 첫 댓글을 달아보세요.</div>}
+        {loadCommentList?.map((comment, i) => (
+          <div key={i} className={styles.comment_component}>
+            <div className={styles.comment_user}>
+              <FaUserCircle className={styles.user_profile_img} />
+              <div className={styles.user_name_comment}>{comment.profileId}</div>
+            </div>
+            <div className={styles.commnet_text}>{comment.comment}</div>
           </div>
-          <div className={styles.commnet_text}>{comment.comment}</div>
-        </div>
-      ))}
-      {moreLoad ? (
-        <button type='button' onClick={handleLoadMore}>
-          더 불러오기
-        </button>
-      ) : (
-        <p>댓글 다 불러왔다냥</p>
-      )}
+        ))}
+      </div>
+      <div className={styles.ref} ref={target} />
     </div>
   );
 };
